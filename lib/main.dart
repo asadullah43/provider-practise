08feb128fbab2fc52206378cede5b72a0ddf9926
asadullah43 <100843918+asadullah43@users.noreply.github.com>
2023-01-23 15:04:02 +0300

    understand statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider_practise/home_screen.dart';
+import 'package:provider_practise/state_full.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: MyStateFull(),
     );
   }
 }
--- lib/state_full.dart
+++ lib/state_full.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
-  int x = 2;
+class MyStateFull extends StatefulWidget {
+  MyStateFull({Key? key}) : super(key: key);
+
+  @override
+  State<MyStateFull> createState() => _MyStateFullState();
+}
+
+class _MyStateFullState extends State<MyStateFull> {
+  int x = 8;
 
   @override
   Widget build(BuildContext context) {
+    print("build");
     return Scaffold(
       appBar: AppBar(title: const Text("Provider Practise")),
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(DateTime.now().toString()),
         Container(
           child: Center(
             child: Text(
@@ -22,6 +30,7 @@ class HomeScreen extends StatelessWidget {
         onPressed: () {
           x++;
           print(x);
+          setState(() {});
         },
         child: const Icon(Icons.add),
       ),
